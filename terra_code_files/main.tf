c09c0c6e58493a44340c3b3d95c3c9cc839aedb1
Parsh <Par@gmail.com>
2024-03-26 23:06:55 +0530

    main.tf updated

--- terra_code_files/main.tf
+++ terra_code_files/main.tf
@@ -1,8 +1,27 @@
+data "local_file" "private_key"{
+    filename = "terra_code_files/privatekey.pem"
+}
 resource "aws_instance" "web"{
     ami = "ami-05295b6e6c790593e"
     instance_type = var.instance_type
     tags = {
         Name = "Hi"
          }
+
+ provisioner "remote-exec" {
+    inline = [ 
+        "sudo yum update -y",
+        "sudo yum install -y ansible",
+        "ansible-playbook -i localhost, -c local orch_tion.yml"
+        ]
+ }
+
+ connection {
+    type  = "ssh"
+    user  = "ec2-user"
+    private_key = data.local.file.private_key.content
+    host = self.public_ip
+    
+ }
 } 
 
